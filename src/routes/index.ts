e2582622cadc640a494e801c765144cb768dfe57
Wladimir <wladimirgrf@gmail.com>
2020-05-22 20:39:00 -0300

    add: Prettier/Eslint config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 
-
 const routes = Router();
 
 routes.get('/', (request, response) => response.json({ message: 'OK' }));
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🖥  Server started on port 3333');
