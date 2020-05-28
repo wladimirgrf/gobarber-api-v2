8cc2b49b2dd071fc8c9bf1f8992704ffa6cda2e3
Wladimir <wladimir.code@gmail.com>
2020-05-27 22:42:45 -0300

    fix: Change User & Appointment id to uuid

--- src/database/migrations/1590455648669-CreateAppointments.ts
+++ src/database/migrations/1590455648669-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1590455648669
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
