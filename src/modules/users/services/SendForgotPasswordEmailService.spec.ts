485c5d3dc2624cf9e9e76c861674656811091836
Wladimir <wladimir.code@gmail.com>
2020-08-16 16:38:33 -0300

    feat: SendForgotPassowrdEmailService, new implementations and tests

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,19 +1,34 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
 
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+
+let fakeMailProvider: FakeMailProvider;
+
+let sendForgotPasswordEmail: SendForgotPasswordEmailService;
+
 describe('SendForgotPassowrdEmail', () => {
-  it('should be able to recover the passowrd using the email', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
+    fakeMailProvider = new FakeMailProvider();
 
-    const sendMailFunction = jest.spyOn(fakeMailProvider, 'sendMail');
-
-    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
-      fakeUsersRepository,
+    sendForgotPasswordEmail = new SendForgotPasswordEmailService(
       fakeMailProvider,
+      fakeUsersRepository,
+      fakeUserTokensRepository,
     );
+  });
+
+  it('should be able to recover the password using the email', async () => {
+    const sendMailFunction = jest.spyOn(fakeMailProvider, 'sendMail');
 
     await fakeUsersRepository.create({
       name: 'Wladimir',
@@ -26,5 +41,32 @@ describe('SendForgotPassowrdEmail', () => {
     });
 
     expect(sendMailFunction).toHaveBeenCalled();
+  });
+
+  it('should not be able to recover a non-existing user password', async () => {
+    await expect(
+      sendForgotPasswordEmail.execute({
+        email: 'wladimir@gmail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should generate a forgot password token', async () => {
+    const generateTokenFunction = jest.spyOn(
+      fakeUserTokensRepository,
+      'generate',
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'Wladimir',
+      email: 'wladimir@gmail.com',
+      password: '12345678',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'wladimir@gmail.com',
+    });
+
+    expect(generateTokenFunction).toHaveBeenCalledWith(user.id);
   });
 });
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,11 @@
 import { injectable, inject } from 'tsyringe';
 
+import AppError from '@shared/errors/AppError';
+
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -10,14 +14,25 @@ interface IRequest {
 @injectable()
 class SendForgotPasswordEmailService {
   constructor(
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('MailProvider')
-    private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvider.sendMail(email, 'Request: pasword recover');
   }
 }
