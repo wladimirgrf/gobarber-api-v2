1d8e848eae9fc588c9562d21dc54e92d36469fed
Wladimir <wladimir.code@gmail.com>
2020-08-25 22:19:54 -0300

    feat: ListProviderDayAvailability, allow only future hours

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -13,10 +13,14 @@ describe('ListProviderDayAvailability', () => {
     );
   });
 
-  it('should be able to list the month availability from provider', async () => {
+  it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentRepository.create({
       providerId: 'provider',
-      date: new Date(2020, 7, 20, 8, 0, 0),
+      date: new Date(2020, 7, 20, 14, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 7, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -28,9 +32,10 @@ describe('ListProviderDayAvailability', () => {
 
     expect(availability).toEqual(
       expect.arrayContaining([
-        { hour: 8, available: false },
-        { hour: 9, available: true },
-        { hour: 10, available: true },
+        { hour: 9, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: true },
       ]),
     );
   });
