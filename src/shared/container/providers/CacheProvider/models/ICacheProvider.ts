5555dc23411e28412c9caf0b24c4e8f20509c3ad
Wladimir <wladimir.code@gmail.com>
2020-09-07 22:47:17 -0300

    feat: Cache, invalidate prefix

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,4 +2,5 @@ export default interface ICacheProvider {
   recover<T>(key: string): Promise<T | null>;
   save(key: string, value: any): Promise<void>;
   invalidate(key: string): Promise<void>;
+  invalidatePrefix(prefix: string): Promise<void>;
 }
